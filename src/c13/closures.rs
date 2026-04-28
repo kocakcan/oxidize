0ce85b465299e8b300a31d35ae2167ce7b9200a9
cankocak <can_kocak1@epam.com>
2026-04-28 22:02:15 +0300

    im so tired

--- src/c13/closures.rs
+++ src/c13/closures.rs
@@ -23,6 +23,29 @@
 /// The store defined in main has two blue shirts and one red shirt remaining to distribute for
 /// this limited-edition promotion. We call the giveaway method for a user with a preference for a
 /// red shirt and a user without any preference.
+///
+/// Again, this code could be implemented in many ways, to focus on closures, we've stuck to
+/// concepts you've already learned, except for the body of the giveaway method that uses a
+/// closure. In the giveaway method, we get the user preference as a parameter of type
+/// Option<ShirtColor> and call the unwrap_or_else method on user_preference. The unwrap_or_else
+/// method on Option<T> is defined by the standard library. It takes one argument: a closure
+/// without any arguments that return a value T (the same type stored in the Some variant of the
+/// Option<T>, in this case ShirtColor). If the Option<T> is the Some variant, unwrap_or_else
+/// returns the value from within the Some. If the Option<T> is the None variant, unwrap_or_else
+/// calls the closure returns the values returned by the closure.
+///
+/// We specify the closure expression || self.most_stocked() as the argument to unwrap_or_else.
+/// This is a closure that takes no parameters itself (if the closure had parameters, they would
+/// appear between the two vertical bars). The body of the closure calls self.most_stocked(). We're
+/// defining the closure here, and the implementation of unwrap_or_else will evaluate the closure
+/// later if the result is needed.
+///
+/// One interesting aspect here is that we've passed a closure that calls self.most_stocked() on
+/// the current Inventory instance. The standard library didn't need to know anything about the
+/// Inventory or ShirtColor types we defined, or the logic we want to use in this scenario. The
+/// closure captures an immutable reference to the self Inventory instance and passes it with the
+/// code we specify to the unwrap_or_else method. Function, on the other hand, are not able to
+/// capture their environment in this way.
 #[derive(Debug, PartialEq, Copy, Clone)]
 enum ShirtColor {
     Red,
