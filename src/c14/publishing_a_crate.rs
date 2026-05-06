83dba5cde01bcf89d3501155bb3a583a8a7dc79b
cankocak <can_kocak1@epam.com>
2026-05-06 20:01:04 +0300

    what the fuck is this

--- src/c14/publishing_a_crate.rs
+++ src/c14/publishing_a_crate.rs
@@ -92,3 +92,81 @@
 ///
 /// Documentation comments within items are useful for describing crates and modules especially. Use them to expplain
 /// the overall purpose of the container to help your users understand the crate's organization.
+///
+/// Exporting a Convenient Public API
+///
+/// The structure of your public API is a major consideration when publishing a crate. People who
+/// use your crate are less familiar with the structure than you are and might have difficulty
+/// finding the pieces they want to use if your crate has a large module hierarchy.
+///
+/// pub keyword is used to make items public and use keyword used to bring items into scope.
+/// However, the structure that makes sense to you while you're developing a crate might not be
+/// very convenient for your users. You might want to organize your structs in a hierarchy might
+/// have trouble finding out that type exists. They might also be annoyed at having to enter use
+/// my_crate::some_module::another_module::UsefulType; rather than use my_crate::UsefulType;
+///
+/// The good news is that if the structure isn't convenient for others to use from another library,
+/// you don't have to rearrange your internal organization: Instead, you can re-export items to
+/// make a public structure that's different from your private structure by using pub use.
+/// Re-exporting takes a public item in one location and makes it public in another location, as if
+/// it were defined in the other location instead.
+///
+/// For example, say we made a library named art for modeling artistic concepts. Within this
+/// library are two modules: a kinds module containing two enums named PrimaryColor and
+/// SecondaryColor and a utils module containing a function named mix, as shown in Listing 14-3.
+//! # Art
+//!
+//! A library for modeling artistic concepts.
+//!
+//! pub mod kinds {
+//!     /// The primary colors according to the RYB color model.
+//!     pub enum PrimaryColor {
+//!         Red,
+//!         Yellow,
+//!         Blue,
+//!     }
+//!
+//!     /// The secondary colors according to the RYB color model.
+//!     pub enum SecondaryColor {
+//!         Orange,
+//!         Green,
+//!         Purple,
+//!     }
+//! }
+//!
+//! pub mod utils {
+//!     use crate::kinds::*;
+//!
+//!     /// Combines two primary colors in equal amounts to create
+//!     /// a secondary color.
+//!     pub fn mix(c1: PrimaryColor, c2: SecondaryColor) -> SecondaryColor {
+//!         // --snip--
+//!     }
+//! }
+//! Listing 14-3: An art library with items organized into kinds and utils modules
+/// Note that the PrimaryColor and SecondaryColor types aren't listed on the front page, nor is the
+/// mix function. We have to click kinds and utils to see them.
+///
+/// Another crate that depends on this library would need use statements that bring the items from
+/// art into scope, specifying the module structure that's currently defined. Listing 14-4 shows an
+/// example of a crate that uses the PrimaryColor and mix items from the art crate.
+///
+///     use art::kinds::PrimaryColor;
+///     use art::utils::mix;
+///
+///     fn main() {
+///         let red = PrimaryColor::Red;
+///         let yellow = PrimaryColor::Yellow;
+///         mix(red, yellow);
+///     }
+///     Listing 14-4: A crate using the art crate's items with its internal structure exported
+/// The author of the code in Listing 14-4, which uses the art crate, had to figure out that
+/// PrimaryColor is in the kinds module and mix is in the utils module. The module structure of the
+/// art crate is more relevant to developers working on the art crate than to those using it. The
+/// internal structure doesn't contain any useful information for someone trying to understand how
+/// to use the art crate, but rather causes confusion because developers who use it have to figure
+/// out where to look, and must specify the module names in the use statement.
+///
+/// To remove the internal organization from the public API, we can modify the art crate code in
+/// Listing 14-3 to add pub use statements to re-export the items at the top level, as shown in
+/// Listing 14-5.
