8405767bea644f964bb4dc25ddc7a1b23a453cb4
cankocak <can_kocak1@epam.com>
2026-05-07 19:07:38 +0300

    completed box

--- src/c15/box.rs
+++ src/c15/box.rs
@@ -58,3 +58,131 @@
 /// Putting a single value on the heap isn't very useful, so you won't use boxes by themselves in
 /// this way very often. Having values like a single i32 on the stack, where they're stored by
 /// default, is more appropriate in the majority of situations.
+///
+/// Enabling Recursive Types with Boxes
+///
+/// A value of a recursive type can have another value of the same type as part of itself.
+/// Recursive types pose an issue because Rust needs to know at compile time how much space a type
+/// takes up. However, the nesting of values of recursive types could theoretically continue
+/// infinitely, so Rust can't know how much space the value needs. Because boxes have a known size,
+/// we can enable recursive types by inserting a box in the recursive type definition.
+///
+/// Understanding the Cons List
+///
+/// A const list is a data structure is made up of nested pairs, and is the Lisp version of a
+/// linked list. Its name comes from the cons (short for construct function) in Lisp that
+/// constructs a new pair from its two arguments. By calling cons on a pair consisting of a value
+/// and another pair, we can construct cons lists made up of recursive pairs.
+///
+///     (1, (2, (3, Nil)))
+/// Each item in a cons list contains two elements: the value of the current item and of the next
+/// item. The last item in the list contains only a value called Nil without a next item. A cons
+/// list is produced by recursively calling the const function. The canonical name to denote the
+/// base case of the recursion is Nil. Note that this is not the same as the "null" or "nil"
+/// concept, which is an invalid or absent value.
+///
+/// The cons list isn't a commonly used data structure in Rust. Most of the time when you have a
+/// list of items in Rust, Vec<T> is a better choice to use. Other, more complex recursive data
+/// types are useful in various situations, but by starting with the cons list we can explore how
+/// boxes let us define a recursive data types without much distraction.
+///
+/// Listing 15-2 contains an enum definition for a cons list. Note that this code won't compile
+/// yet, because the List type doesn't have a known size.
+///
+///     enum List {
+///         Cons(i32, List),
+///         Nil,
+///     }
+///     Listing 15-2: The first attempt at defining an enum to represent a cons list data structure
+///     of i32 values
+/// This cons list could have been implemented over generic types. Using the List type to store the
+/// list 1, 2, 3 would look like the code in Listing 15-3.
+///
+///     use crate::List::{Cons, Nil};
+///
+///     fn main() {
+///         let list = Cons(1, Cons(2, Cons(3, Nil)));
+///     }
+///     Listing 15-3: Using the List enum to store the list 1, 2, 3
+/// The first Cons value holds 1 and another List value. This List value is another Cons value that
+/// holds 2 and another List value. This List value is one more Cons value that hold 3 and a List
+/// value, which is finally Nul, the non-recursive variant that signals the end of the list.
+///
+///     error: recursive type `List` has infinite size
+/// The error shows this type "has infinite size." The reason is that we've defined List with a
+/// variant that is recursive: It holds another value of itself directly. As a result, Rust can't
+/// figure out how much space it needs to store a List value.
+///
+/// Computing the Size of a Non-Recursive Type
+///
+///     enum Message {
+///         Quit,
+///         Move { x: i32, y: i32 },
+///         Write(String),
+///         ChangeColor(i32, i32, i32),
+///     }
+/// To determine how much space to allocate for a Message value, Rust goes through each of the
+/// variants to see which variants needs the most space. Rust sees that Message::Quit doesn't need
+/// any space, Message::Move needs enough space to store two i32 values, and so forth. Because only
+/// one variant will be used, the most space a Message value will need is the space it would take
+/// to store the largest of its variants.
+///
+/// Contrast this with what happens when Rust tries to determine how much space a recursive type
+/// like the List enum in Listing 15-2 needs. The compiler starts by looking at the Cons variant,
+/// which holds a value of type i32 and a value of type List. Therefore, Cons needs an amount of
+/// space equal to the size of an i32 plus the size of a List. To figure out how much memory the
+/// List type needs, the compiler looks at the variants, starting with the Cons variant. The Cons
+/// variant hold a value of type i32 and a value of type List, and this process continues
+/// infinitely.
+///
+/// Getting a Recursive Type with a Known Size
+///
+/// Because Rust can't figure out how much space to allocate for recursively defined types, the
+/// compiler gives an error with this helpful suggestion:
+///
+///     help: insert some indirection (e.g., a `Box`, `Rc`, or `&`) to break the cycle
+/// In this suggestion, indirection means that instead of storing a value directly, we should
+/// change the data structure to store the value indirectly by storing a pointer to the value instead.
+///
+/// Because a Box<T> is a pointer, Rust always knows how much space a Box<T> needs: A pointer's
+/// size doesn't change based on the amount of data it's pointing to. This means we can put a
+/// Box<T> inside the Cons variant instead of another List value directly. The Box<T> will point to
+/// the next List value that will be on the heap rather than inside the Cons variant. Conceptually,
+/// we still have a list created with lists holding other lists, but this implementation is now
+/// more like placing the items next to one another rather than inside one another.
+///
+///     enum List {
+///         Cons(i32, Box<List>),
+///         Nil,
+///     }
+///
+///     use crate::List::{Cons, Nil};
+///
+///     fn main() {
+///         let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+///     }
+///     Listing 15-5: The definition of List that uses Box<T> in order to have a known size
+/// The Cons variant needs the size of an i32 plus the space to store the box's pointer data. The
+/// Nil variant stores no values, so it needs less space on the stack than the Cons variant. We
+/// know how that any List value will take up the size of an i32 plus the size of a box's pointer
+/// data. By using a box, we've broken the infinite, recursive chain, so the compiler can figure
+/// out the size it needs to store a List value.
+///
+/// Boxes provide only the indirection and heap allocation; they don't have any other special
+/// capabilities. They also don't have the performance overhead that these special capabilities
+/// incur, so they can be useful in cases like the cons list where the indirection is the only
+/// feature we need.
+///
+/// The Box<T> type is a smart pointer because it implements the Deref trait, which allows Box<T>
+/// values to be treated like references. When a Box<T> values goes out of scope, the heap data
+/// that the box is pointing to is cleaned up as well because of the Drop trait implementation.
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+
+fn main() {
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+}
