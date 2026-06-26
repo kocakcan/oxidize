0dfbb1b7146ad0aa7ef7e140d217162ecf4c4c31
kocakcan <can_kocak1@epam.com>
2026-06-26 09:26:59 +0300

    started advanced features

---
+++ src/c20/advanced_features.rs
@@ -0,0 +1,5 @@
+///                                 Advanced Features
+///
+/// Unsafe Rust
+/// Safe Rust is conservative by design, it rejects valid programs when it can't prove correctness.
+/// unsafe is the escape hatch for when you know better than the compiler.
